b20369d63669be792700efbf6d946db41680f269
DanDuarte24 <162132350+DanDuarte24@users.noreply.github.com>
2025-07-07 08:50:17 -0300

    Delete factorymethod directory

--- factorymethod/CriadorPersonagem.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.ads.padroes.rpg.factorymethod;
-
-import br.edu.ifpb.ads.padroes.rpg.model.Personagem;
-
-public interface CriadorPersonagem {
-    Personagem criar(String nome, String classe);
-}
